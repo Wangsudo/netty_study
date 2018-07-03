3247f4bacca277bbee72f5240c61158d002e6673
ws <ws@viroyal.cn>
2018-07-03 16:38:48 +0800

    [netty_study]

---
+++ netty/src/main/java/com/example/netty/bio/TimeServer.java
@@ -0,0 +1,42 @@
+package com.example.netty.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/** @Author: wangshun @Date: Create in 2018-07-02 11:36 */
+public class TimeServer {
+  public static void main(String[] args) {
+    int port = 8080;
+    if (args != null && args.length > 0) {
+      try {
+        port = Integer.valueOf(args[0]);
+      } catch (NumberFormatException e) {
+        e.printStackTrace();
+      }
+    }
+    ServerSocket server = null;
+    try {
+      server = new ServerSocket(port);
+      System.out.println("The time server is start in port：" + port);
+      Socket socket = null;
+      while (true) {
+        socket = server.accept();
+        new Thread(new TimeServerHandler(socket)).start();
+      }
+    } catch (IOException e) {
+
+        e.printStackTrace();
+    } finally {
+        if (server != null){
+            System.out.println("The time server close");
+            try {
+                server.close();
+                server = null;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+  }
+}
